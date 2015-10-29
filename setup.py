f538900a5107a3312f9d8e63daa3e4314ca84281
Ashok Meenakshi Sundaram <mashoksc@gmail.com>
2015-10-29 18:59:00 +0100

    add readme as long description

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(*filenames, **kwargs):
             buf.append(f.read())
     return sep.join(buf)
 
-long_description = read('README')
+long_description = read('README.md')
 
 class PyTest(TestCommand):
     def finalize_options(self):
